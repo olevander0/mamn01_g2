bc693c139e83d45fbb815f1b1807e5bdddf2397f
Angelica Catherine Larsson <an7061la-s@student.lu.se>
2026-02-24 11:29:58 +0100

    Adds TimerActivity and dial.xml

--- app/src/main/java/com/example/mamn01_g2/TimerActivity.java
+++ app/src/main/java/com/example/mamn01_g2/TimerActivity.java
@@ -1,8 +1,7 @@
 package com.example.mamn01_g2;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,22 +9,22 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class TimerActivity extends AppCompatActivity {
+    private ImageView ivDial;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_timer);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        ivDial = findViewById(R.id.iv_dial);
     }
 
-    private void launchTimer(View view) {
-        Intent intent = new Intent(this, TimerActivity.class);
-        startActivity(intent);
-    }
+
 }
