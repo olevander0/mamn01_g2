0e323e020b678cccce0cfb9cf7161217f4575299
Ellen Persson <ellenamalia@outlook.com>
2026-05-08 16:47:54 +0200

    Adds warning when phone is turned up during focus time

--- app/src/main/java/com/example/mamn01_g2/ui/MainActivity.java
+++ app/src/main/java/com/example/mamn01_g2/ui/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.mamn01_g2.ui;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.content.ContextCompat;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.mamn01_g2.R;
@@ -12,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
     private TimePicker timePicker;
     private TimerViewModel viewModel;
     private ClockView clockView;
+
+    private AlertDialog alertDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,6 +55,9 @@ public class MainActivity extends AppCompatActivity {
 
             timePicker.setSelectedSeconds((int) (timeInMillis / 1000));
         });
+        viewModel.getLockInState().observe(this, isLockedIn -> {
+            updateUILockIn(isLockedIn);
+        });
     }
 
     @Override
@@ -64,6 +73,47 @@ public class MainActivity extends AppCompatActivity {
         super.onPause();
         if (viewModel != null) {
             viewModel.stopSensors();
+        }
+    }
+
+    private void updateUILockIn(boolean isLockedIn) {
+        ConstraintLayout rootLayout = findViewById(R.id.main);
+
+        if (isLockedIn) {
+            showWarningDialog();
+            rootLayout.setBackgroundColor(
+                    ContextCompat.getColor(this, android.R.color.holo_red_dark)
+            );
+        } else {
+            hideWarningDialog();
+            rootLayout.setBackgroundColor(
+                    ContextCompat.getColor(this, R.color.background_color)
+            );
+        }
+    }
+
+    private void showWarningDialog() {
+        if (alertDialog == null || !alertDialog.isShowing()) {
+            alertDialog = new AlertDialog.Builder(this)
+                    .setTitle("LOCK IN!!!")
+                    .setMessage("Turn phone facedown")
+                    .setCancelable(false)
+                    .create();
+            alertDialog.show();
+            if (alertDialog.getWindow() != null) {
+                WindowManager.LayoutParams params =
+                        alertDialog.getWindow().getAttributes();
+
+                params.y = -300; // negative = move upward
+
+                alertDialog.getWindow().setAttributes(params);
+            }
+        }
+    }
+
+    private void hideWarningDialog() {
+        if (alertDialog != null && alertDialog.isShowing()) {
+            alertDialog.dismiss();
         }
     }
 }
